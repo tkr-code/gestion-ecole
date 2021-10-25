0c73f3c92db71cd1d13efb889a851cbece194e39
malick tkr <malick.tounkara.1@gmiail.com>
2021-10-25 15:55:53 +0000

    first commit

---
+++ src/Controller/Main/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller\Main;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index(): Response
+    {
+        return $this->render('main/home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}
